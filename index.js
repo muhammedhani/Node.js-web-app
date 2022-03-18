13bec5a2a6e3467e3f69abb6bf3a74dbdbb52e9f
muhammedhani <48108210+muhammedhani@users.noreply.github.com>
2022-03-18 21:01:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,6 @@ const app = express();
 const Joi = require('joi');
 const func = require('./helpful_functions');
 
-// ********************************** PUT Request **********************************
-app.put('/api/games/:id', (req, res) => {
-	const game = games.find((c) => c.id === parseInt(req.params.id));
-	if (!game) {
-		res.status(404).send('The game with this id was not found...');
-		return;
-	}
-
-	// Input Validation with joi package
-	const schema = {
-		genre: Joi.string().required(),
-		name: Joi.string().min(3).required(),
-	};
-	const result = Joi.validate(req.body, schema);
-	if (result.error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
-
-	game.genre = req.body.genre;
-	game.name = req.body.name;
-	res.send(game);
-});
-// ****************************************************************************************
-
-
 app.use(express.json());
 
 const games = [
@@ -50,6 +24,31 @@ app.get('/api/games/:id', (req, res) => {
 		res.status(404).send('The game was not found...');
 		return;
 	}
+	res.send(game);
+});
+// ****************************************************************************************
+
+// ********************************** PUT Request **********************************
+app.put('/api/games/:id', (req, res) => {
+	const game = games.find((c) => c.id === parseInt(req.params.id));
+	if (!game) {
+		res.status(404).send('The game with this id was not found...');
+		return;
+	}
+
+	// Input Validation with joi package
+	const schema = {
+		genre: Joi.string().required(),
+		name: Joi.string().min(3).required(),
+	};
+	const result = Joi.validate(req.body, schema);
+	if (result.error) {
+		res.status(400).send(error.details[0].message);
+		return;
+	}
+
+	game.genre = req.body.genre;
+	game.name = req.body.name;
 	res.send(game);
 });
 // ****************************************************************************************
