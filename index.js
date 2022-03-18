2c8bd34b802b71806e17122f1ab9e9b9a46c499d
mohamed-ali-5050 <mohamedalimohamed5050@gmail.com>
2022-03-18 21:14:57 +0200

    adding POST Request

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const app = express();
 const Joi = require('joi');
 const func = require('./helpful_functions');
 
+app.use(express.json());
+
+const games = [
+	{ id: 1, genre: 'Sport', name: 'FIFA 22' },
+	{ id: 2, genre: 'Action', name: 'Call of Duty' },
+	{ id: 3, genre: 'Battle Royal', name: 'PUBG' },
+];
+
+// ********************************** GET Request **********************************
+// Get all games
+app.get('/api/games', (req, res) => {
+	res.send(games);
+});
+
+// Get by id -> "/api/games/id"
+app.get('/api/games/:id', (req, res) => {
+	const game = games.find((c) => c.id === parseInt(req.params.id));
+	if (!game) {
+		res.status(404).send('The game was not found...');
+		return;
+	}
+	res.send(game);
+});
+// ****************************************************************************************
 // ********************************** PUT Request **********************************
 app.put('/api/games/:id', (req, res) => {
 	const game = games.find((c) => c.id === parseInt(req.params.id));
@@ -28,32 +52,31 @@ app.put('/api/games/:id', (req, res) => {
 });
 // ****************************************************************************************
 
+// ********************************** POST Request **********************************
+app.post('/api/games', (req, res) => {
+	// check input Validation with joi package
+	const schema = {
+		genre: Joi.string().required(),
+		name: Joi.string().min(3).required(),};
 
-app.use(express.json());
-
-const games = [
-	{ id: 1, genre: 'Sport', name: 'FIFA 22' },
-	{ id: 2, genre: 'Action', name: 'Call of Duty' },
-	{ id: 3, genre: 'Battle Royal', name: 'PUBG' },
-];
-
-// ********************************** GET Request **********************************
-// Get all games
-app.get('/api/games', (req, res) => {
-	res.send(games);
-});
-
-// Get by id -> "/api/games/id"
-app.get('/api/games/:id', (req, res) => {
-	const game = games.find((c) => c.id === parseInt(req.params.id));
-	if (!game) {
-		res.status(404).send('The game was not found...');
+	const result = Joi.validate(req.body, schema);
+	if (result.error) 
+	{
+		res.status(400).send(error.details[0].message);
 		return;
 	}
-	res.send(game);
-});
-// ****************************************************************************************
 
+	const game =
+	 {
+		id: games.length + 1,
+		genre: req.body.genre,
+		name: req.body.name,
+	};
+	games.push(game);
+	res.send(game);
+}
+);
+// ****************************************************************************************
 // PORTs
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
