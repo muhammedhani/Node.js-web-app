d27be0bac40f5bf0c3c0a65227f8a186fe71bf4d
muhammedhani <48108210+muhammedhani@users.noreply.github.com>
2022-03-18 21:41:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/api/games/:id', (req, res) => {
 	res.send(game);
 });
 // ****************************************************************************************
+
 // ********************************** PUT Request **********************************
 app.put('/api/games/:id', (req, res) => {
 	const game = games.find((c) => c.id === parseInt(req.params.id));
@@ -74,31 +75,6 @@ app.post('/api/games', (req, res) => {
 	res.send(game);
 }
 );
-// ****************************************************************************************
-
-// ********************************** PUT Request **********************************
-app.put('/api/games/:id', (req, res) => {
-	const game = games.find((c) => c.id === parseInt(req.params.id));
-	if (!game) {
-		res.status(404).send('The game with this id was not found...');
-		return;
-	}
-
-	// Input Validation with joi package
-	const schema = {
-		genre: Joi.string().required(),
-		name: Joi.string().min(3).required(),
-	};
-	const result = Joi.validate(req.body, schema);
-	if (result.error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
-
-	game.genre = req.body.genre;
-	game.name = req.body.name;
-	res.send(game);
-});
 // ****************************************************************************************
 
 // PORTs
